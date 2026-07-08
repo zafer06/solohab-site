3e92c299fff7a0481aa51370a45c183c53afff02
Zafer Lenovo <zafercelenk@gmail.com>
2026-07-08 16:04:39 +0300

    Tailwind tamamen kaldırıldı ve klasik css yapısına geçildi

--- static/js/site.js
+++ static/js/site.js
@@ -1,13 +1,1 @@
-tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                brand: "#d63939",
-                brandDark: "#b52f2f",
-            },
-            fontFamily: {
-                sans: ["Inter", "sans-serif"],
-            },
-        },
-    },
-};
+// js file
